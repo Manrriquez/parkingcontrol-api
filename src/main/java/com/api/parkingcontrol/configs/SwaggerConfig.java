0feb0fe5b3354708d24b6bff7df45e3986ecdf0e
Manrriquez <manrriquez.contato@gmail.com>
2022-11-09 19:35:05 -0300

    adicionando documentação com swagger

---
+++ src/main/java/com/api/parkingcontrol/configs/SwaggerConfig.java
@@ -0,0 +1,35 @@
+package com.api.parkingcontrol.configs;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+
+//@EnableSwagger2
+@Configuration
+public class SwaggerConfig {
+
+//    @Bean
+//    public Docket api() {
+//        return new Docket(DocumentationType.SWAGGER_2)
+//                .select()
+//                .apis(RequestHandlerSelectors.basePackage("com.swagger.demo"))
+//                .paths(PathSelectors.any())
+//                .build();
+//    }
+//
+//
+//    @Override
+//    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+//
+//        // enabling swagger-ui part for visual documentation
+//        registry.addResourceHandler("swagger-ui.html").addResourceLocations("classpath:/META-INF/resources/");
+//        registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
+//    }
+}
