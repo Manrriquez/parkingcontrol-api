360455701bb557e81a640f4790918f09af1c1e7c
Manrriquez <manrriquez.contato@gmail.com>
2022-12-13 22:18:51 -0300

    implementando controle de acesso por meio do spring security

--- src/main/java/com/api/parkingcontrol/configs/security/WebSecurityConfig.java
+++ src/main/java/com/api/parkingcontrol/configs/security/WebSecurityConfig.java
@@ -4,6 +4,7 @@ package com.api.parkingcontrol.configs.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -21,6 +22,9 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         http.httpBasic()
                 .and()
                 .authorizeHttpRequests()
+                .antMatchers(HttpMethod.GET, "/parking-spot/**").permitAll()
+                .antMatchers(HttpMethod.POST, "/parking-spot").hasRole("USER")
+                .antMatchers(HttpMethod.DELETE, "/parking-spot").hasRole("ADMIN")
                 .anyRequest()
                 .authenticated()
                 .and()
